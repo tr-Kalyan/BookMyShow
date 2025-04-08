593e65f18173032afe634b9e311b6b20f8001d44
Kalyan TR <kalyantr@Kalyans-Mac-Studio.local>
2025-04-08 23:40:09 +0530

    db auth schema configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,4 +2,13 @@ const mongoose = require('mongoose');
 
 const dbURL = process.env.DB_URL
 
-console.log(dbURL)
+const connectDB = async() =>{
+    try{
+        await mongoose.connect(dbURL)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = connectDB;
---
+++ backend/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+//user account schema
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        email:['admin','user','partner'],
+        required:true,
+        default:"user"
+    }
+})
+
+const userModel = mongoose.model(user,userSchema);
+
+module.exports = userModel;
